883edced77de7039b0483709a605f1f5c2fa2d07
LJH4042 <lejh5940@gmail.com>
2025-08-13 18:34:41 +0900

    Lv.0 21 ~ 30

---
+++ Lv.0/28.n의배수고르기.js
@@ -0,0 +1,13 @@
+//정수 n과 정수 배열 numlist가 매개변수로 주어질 때, numlist에서 n의 배수가 아닌 수들을 제거한 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(n, numlist) {
+  return numlist.filter((num) => num % n == 0); //배열.filter() : 원하는 조건에 따라 배열을 필터링하는 함수
+}
+
+const result1 = solution(3, [4, 5, 6, 7, 8, 9, 10, 11, 12]);
+const result2 = solution(5, [1, 9, 3, 10, 13, 5]);
+const result3 = solution(12, [2, 100, 120, 600, 12, 12]);
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
